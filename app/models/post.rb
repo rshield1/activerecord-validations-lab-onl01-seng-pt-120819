e9e7894f24ea6f752d8439dcaf3fc27e6ef24531
Robert Shields <robert.shieldsiv@gmail.com>
2020-05-02 20:45:46 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,18 +13,13 @@ class Post < ActiveRecord::Base
     if title
 end
 
+CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
 
-# validate :expiration_date_cannot_be_in_the_past,
-#     :discount_cannot_be_greater_than_total_value
- 
-#   def expiration_date_cannot_be_in_the_past
-#     if expiration_date.present? && expiration_date < Date.today
-#       errors.add(:expiration_date, "can't be in the past")
-#     end
-#   end
- 
-#   def discount_cannot_be_greater_than_total_value
-#     if discount > total_value
-#       errors.add(:discount, "can't be greater than total value")
-#     end
-#   end
+  def is_clickbait?
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
