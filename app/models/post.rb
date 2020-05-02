a8a05cc2bd797da570a82082d8093abc201c6a36
Robert Shields <robert.shieldsiv@gmail.com>
2020-05-02 20:44:04 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,26 @@ class Post < ActiveRecord::Base
   validates :content, :length { minimum: 250 }
   validates :summary, :length { maximum: 250 }
   validates :category, inclusion: { in: %w(fiction nonfiction)}
+  
+  
+  validates :clickbait_title
+  
+  def clickbait_title
+    if title
 end
+
+
+# validate :expiration_date_cannot_be_in_the_past,
+#     :discount_cannot_be_greater_than_total_value
+ 
+#   def expiration_date_cannot_be_in_the_past
+#     if expiration_date.present? && expiration_date < Date.today
+#       errors.add(:expiration_date, "can't be in the past")
+#     end
+#   end
+ 
+#   def discount_cannot_be_greater_than_total_value
+#     if discount > total_value
+#       errors.add(:discount, "can't be greater than total value")
+#     end
+#   end
