8a2bb6e128b844c304478ee7975fd6ed29bca5d6
Robert Shields <robert.shieldsiv@gmail.com>
2020-05-02 20:52:42 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,10 @@ class Post < ActiveRecord::Base
   
   
   validates :title, presence: true
-  validates :content, :length { minimum: 250 }
-  validates :summary, :length { maximum: 250 }
-  validates :category, inclusion: { in: %w(fiction nonfiction)}
-  validates :clickbait_title
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbait_title?
   
   CLICKBAIT_WORDS = [
     /Won't Believe/i,
